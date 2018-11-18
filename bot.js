8740ab718bcf0a31ceb737d4d634189ac0a4a0dc
multo11 <mchereiam8@gmail.com>
2018-11-18 15:52:45 +0100

    sa

--- bot.js
+++ bot.js
@@ -8,15 +8,15 @@ client.on("ready", () => {
     })
 })
 
-client.on('message',async message => {
+client.on('message'if(message.author.bot) return;,async message => {
     if (message.content == "!avatar") {
     message.reply("Oto twój Avatar "+message.author.avatarURL)
     }
     if (message.content == "ping") {
         message.channel.send("pong")
     }
-    if (message.content =="Jak sie nazywasz bocie"){
-        message.channel.send("Chetoo")
+    if (message.content =="pong"){
+        message.channel.send("ping")
     }
 })
  
