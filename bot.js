887fa461f1c3adf48a80bbf320b2e717dbadacc3
multo11 <mchereiam8@gmail.com>
2018-11-18 15:47:45 +0100

    Dodano komendy pong

--- bot.js
+++ bot.js
@@ -15,7 +15,9 @@ client.on('message',async message => {
     if (message.content == "ping") {
         message.channel.send("pong")
     }
-
+    if (message.content =="pong"){
+        message.channel.send("ping")
+    }
 })
  
     
