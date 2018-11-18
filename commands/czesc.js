8db1a87f7b7fb0561a2bfd38d5e9d5be5e4a639a
multo11 <mchereiam8@gmail.com>
2018-11-18 19:37:50 +0100

    Kolejna litera

--- commands/czesc.js
+++ commands/czesc.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args, config) => {
-message.reply("cześć")
+message.reply("Cześć")
 }
 module.exports.help = {
   name: "cześć",
