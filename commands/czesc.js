7f0fb8f19bc4b257b303d8a9b7daefa747bedd05
multo11 <mchereiam8@gmail.com>
2018-11-18 19:35:09 +0100

    dasdad

---
+++ commands/czesc.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+module.exports.run = async (client, message, args, config) => {
+message.reply("czesc")
+}
+module.exports.help = {
+  name: "czesc",
+  category: "util"
+}
